6aeefb96c325eecd28686efeef82b992277a98f2
Anton Senderkin <cortezguitar@gmail.com>
2020-07-12 19:33:36 +0300

    Updates

---
+++ precache-manifest.f4728167cb46bee778470001b1e1147d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526eb54be8c24bff172794c75e02899c",
+    "url": "/airpods/index.html"
+  },
+  {
+    "revision": "05b2a49460c471682c02",
+    "url": "/airpods/static/css/main.96ea0529.chunk.css"
+  },
+  {
+    "revision": "16aa3d3acf53ef293e77",
+    "url": "/airpods/static/js/2.98712555.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/airpods/static/js/2.98712555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b2a49460c471682c02",
+    "url": "/airpods/static/js/main.a201bec0.chunk.js"
+  },
+  {
+    "revision": "1ce714b298d494478cbb",
+    "url": "/airpods/static/js/runtime-main.bcaff6cf.js"
+  },
+  {
+    "revision": "305e85fccaba0de2cf7c456b045454ce",
+    "url": "/airpods/static/media/airpods1.305e85fc.jpg"
+  },
+  {
+    "revision": "78b7cdffa5770537632d31d87023d18f",
+    "url": "/airpods/static/media/airpods1_item.78b7cdff.jpg"
+  },
+  {
+    "revision": "265fedabfca0378d42912ed3738a2782",
+    "url": "/airpods/static/media/airpods2_item.265fedab.jpg"
+  },
+  {
+    "revision": "b5e1a099efda0ea083587b8e6e8ca9f3",
+    "url": "/airpods/static/media/airpodspro_item.b5e1a099.jpg"
+  },
+  {
+    "revision": "433f1c2311ad1b0167543793d46a4e6f",
+    "url": "/airpods/static/media/delivery.433f1c23.jpg"
+  },
+  {
+    "revision": "ae32a51246ce73989af5933b1ae76928",
+    "url": "/airpods/static/media/gear.ae32a512.svg"
+  },
+  {
+    "revision": "e577759e274969c7bdcf2ef9f6b580b5",
+    "url": "/airpods/static/media/medal.e577759e.png"
+  },
+  {
+    "revision": "f0737811de6902cf171c3f9228d338b1",
+    "url": "/airpods/static/media/protection.f0737811.jpg"
+  }
+]);
